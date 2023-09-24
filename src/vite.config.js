92ec077c370e64420e0818ea23d96117f524a489
carlos <cbdev2021@gmail.com>
2023-09-24 16:59:56 -0300

    url prod

--- src/vite.config.js
+++ src/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         //target: 'https://mern-b1.onrender.com/', // funciona react local hacia render
         target: 'https://mern-b1-vrcl.vercel.app/', // funciona react local hacia vercel
         //target: 'http://localhost:10000/api/users' || process.env.VERCEL_API_URL,
-        
+      
         //target: 'http://localhost:10000',       // aputando a server local
         //target: process.env.REACT_APP_API_URL, // no funciona en local
         changeOrigin: true,
