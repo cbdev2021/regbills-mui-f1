c9abf5ce63264c370780d562f8e2e741ebe507d2
carlos <cbdev2021@gmail.com>
2023-10-10 01:49:30 -0300

    se agrega pantalla registers y endpoint add

--- src/pages/AddRegister.tsx
+++ src/pages/AddRegister.tsx
@@ -1,22 +1,219 @@
-import { FunctionComponent } from "react";
-import styles from "./AddRegister.module.css";
-import { Container, CssBaseline, Typography } from "@mui/material";
-
+import React, { FunctionComponent, useState, useEffect } from "react";
+import styles from "./Config.module.css";
+import {
+  Container,
+  CssBaseline,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Slide,
+  TextField, // Importamos TextField
+} from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PaidIcon from "@mui/icons-material/Paid";
+import TableConfig from "./TableConfig";
+import {
+  useGetTypeValuesByUserIdQuery,
+} from '../slices/typeValuesApiSlice';
+import {
+  // useDeleteTypeValueMutation,
+  // useUpdateTypeValueMutation,
+  // useAddTypeValueMutation,
+  useAddRegisterMutation, // En lugar de useAddUserMutation
+ // useUpdateRegisterMutation, // En lugar de useUpdateUserMutation
+ // useDeleteRegisterMutation, // En lugar de useDeleteUserMutation
+  //useGetRegisterByIdQuery, // En lugar de useGetUserByIdQuery
+  //useGetRegistersByCriteriaQuery, // En lugar de useGetUsersByCriteriaQuery
+} from '../slices/registerApiSlice';
+import { useSelector } from "react-redux";
+import TableAddRegister from "./TableAddRegister";
 
 const AddRegister: FunctionComponent = () => {
-  return (
-    <Container component="main" maxWidth="xs" sx={{ marginTop: 10, height: '883 px' }}>
-        <CssBaseline />
-        <div>
-          <Typography variant="h5" align="center" gutterBottom>
-          AddRegister
-          </Typography>   
+  const [openDialog, setOpenDialog] = useState(false);
+  const [dialogTitle, setDialogTitle] = useState("");
+  const [spentData, setSpentData] = useState([]);
+  const [incomeData, setIncomeData] = useState([]);
+  const [numericValue, setNumericValue] = useState(""); // Estado para el valor numérico
+  //const [deleteTypeValue] = useDeleteRegisterMutation();
+ // const [updateTypeValue] = useUpdateRegisterMutation();
+  const [addTypeValueMutation] = useAddRegisterMutation();
+  //const [numericKey, setNumericKey] = useState(""); 
 
-          <form className={styles.addregister}>
-             
-          </form>
-        </div>
-  </Container>
+  const userId = useSelector((state: any) => state.auth.userInfo._id);
+  const token = useSelector((state: any) => state.auth.token);
+
+  const { data: dataResponse, refetch } = useGetTypeValuesByUserIdQuery({
+    idUsuario: userId,
+    token: token,
+  });
+
+  // useEffect(() => {
+  //   if (dataResponse) {
+  //     const spentDataMapped = dataResponse.filter((item: { typevalue: string; }) => item.typevalue === 'Spent').map((item: { _id: any; subtype: any; description: any; typevalue: any; }) => ({
+  //       _id: item._id,
+  //       subtype: item.subtype,
+  //       description: item.description,
+  //       typevalue: item.typevalue
+  //     }));
+
+  //     const incomeDataMapped = dataResponse.filter((item: { typevalue: string; }) => item.typevalue === 'Income').map((item: { _id: any; subtype: any; description: any; typevalue: any; }) => ({
+  //       _id: item._id,
+  //       subtype: item.subtype,
+  //       description: item.description,
+  //       typevalue: item.typevalue
+  //     }));
+
+  //     setSpentData(spentDataMapped);
+  //     setIncomeData(incomeDataMapped);
+  //   }
+  // }, [dataResponse]);
+  useEffect(() => {
+    if (dataResponse) {
+      // const spentDataMapped = dataResponse
+      //   .filter((item: { typevalue: string; }) => item.typevalue === 'Spent')
+      //   .map((item: { subtype: any; }) => item.subtype); // Aquí solo seleccionamos el campo "subtype"
+
+      // const incomeDataMapped = dataResponse
+      //   .filter((item: { typevalue: string; }) => item.typevalue === 'Income')
+      //   .map((item: { subtype: any; }) => item.subtype); // Aquí solo seleccionamos el campo "subtype"
+
+      const spentDataMapped = dataResponse
+        .filter((item: { typevalue: string; }) => item.typevalue === 'Spent')
+        .map((item: { _id: string; subtype: any; }) => ({
+          _id: item._id,
+          subtype: item.subtype
+        }));
+
+      const incomeDataMapped = dataResponse
+        .filter((item: { typevalue: string; }) => item.typevalue === 'Income')
+        .map((item: { _id: string; subtype: any; }) => ({
+          _id: item._id,
+          subtype: item.subtype
+        }));
+
+
+
+
+      console.log('spentDataMapped');
+      console.log(spentDataMapped);
+      console.log('incomeDataMapped');
+      console.log(incomeDataMapped);
+
+
+
+      setSpentData(spentDataMapped);
+      setIncomeData(incomeDataMapped);
+    }
+  }, [dataResponse]);
+
+
+
+
+  const handleClickOpen = (title: string) => {
+    setDialogTitle(title);
+    setOpenDialog(true);
+    setNumericValue(""); // Establecer el valor inicial en blanco cuando se abre el diálogo.
+  };
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+
+  const updateData = (newData: any, dataType: any) => {
+    if (dataType === "Spent") {
+      setSpentData(newData);
+    } else if (dataType === "Income") {
+      setIncomeData(newData);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs" sx={{ marginTop: 10, height: '540.5px' }}>
+      <CssBaseline />
+      <div>
+        <Typography variant="h5" align="center" gutterBottom>
+          Add Register
+        </Typography>
+
+        <form className={styles.config}>
+          <div className={styles.buttonsContainer}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={styles.button}
+              onClick={() => handleClickOpen("Spent")}
+              startIcon={<ShoppingCartIcon />}
+            >
+              New Spent
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={styles.button}
+              onClick={() => handleClickOpen("Income")}
+              startIcon={<PaidIcon />}
+            >
+              New Income
+            </Button>
+          </div>
+
+          <Dialog
+            open={openDialog}
+            TransitionComponent={Slide}
+            keepMounted
+            onClose={handleClose}
+            maxWidth="xs"
+            fullWidth
+          >
+            <DialogTitle>{dialogTitle}</DialogTitle>
+            <DialogContent style={{ maxHeight: 400, overflowY: 'scroll' }}>
+              {dialogTitle === "Spent" && (
+                <>
+
+                  <TableAddRegister
+                    userId={userId}
+                    title={dialogTitle}
+                    typevalue="Spent"
+                    data={spentData}
+                    //updateTypeValue={updateTypeValue}
+                    addTypeValueMutation={addTypeValueMutation}
+                    //deleteTypeTypevalueMutation={deleteTypeValue}
+                    token={token}
+                    updateData={updateData}
+                    refetch={refetch}
+                  />
+                </>
+              )}
+              {dialogTitle === "Income" && (
+                <>
+
+                  <TableAddRegister
+                    userId={userId}
+                    title={dialogTitle}
+                    typevalue="Income"
+                    data={incomeData}
+                    //updateTypeValue={updateTypeValue}
+                    addTypeValueMutation={addTypeValueMutation}
+                    //deleteTypeTypevalueMutation={deleteTypeValue}
+                    token={token}
+                    updateData={updateData}
+                    refetch={refetch}
+                  />
+                </>
+              )}
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cerrar
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </form>
+      </div>
+    </Container>
   );
 };
 
---
+++ src/pages/TableAddRegister.tsx
@@ -0,0 +1,276 @@
+import React, { FunctionComponent, useState, useEffect, useRef } from "react";
+import {
+  Typography,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Button,
+  Grid,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { toast } from "react-toastify";
+
+interface TableConfigProps {
+  userId: string;
+  title: string;
+  data: {
+    _id: string;
+    subtype: string;
+  }[];
+  typevalue: string;
+  addTypeValueMutation: any;
+  token: string;
+  updateData: (newData: any, dataType: string) => void;
+  refetch: () => void;
+}
+
+const TableAddRegister: FunctionComponent<TableConfigProps> = ({
+  userId,
+  title,
+  data,
+  typevalue,
+  addTypeValueMutation,
+  token,
+  updateData,
+  refetch,
+}) => {
+  const [editingId, setEditingId] = useState<string | null>(null);
+  const [newSubtype, setNewSubtype] = useState("");
+  const [originalSubtype, setOriginalSubtype] = useState("");
+  const [addNewSubtype, setAddNewSubtype] = useState("");
+  const [isNumericKeyboardOpen, setIsNumericKeyboardOpen] = useState(true);
+  const [monto, setMonto] = useState("");
+  const tableRef = useRef<HTMLTableElement | null>(null);
+  const [fecha, setFecha] = useState("");
+  const [descRegistro, setDescRegistro] = useState("");
+
+  const handleEdit = (id: string) => {
+    setEditingId(id);
+    const currentItem = data.find((item) => item._id === id);
+    if (currentItem) {
+      setNewSubtype(currentItem.subtype);
+      setOriginalSubtype(currentItem.subtype);
+    }
+  };
+
+  const handleAdd = async () => {
+    if (descRegistro.trim() !== "") {
+      try {
+        const response = await addTypeValueMutation({
+          registro: {
+            tipoRegistro: typevalue,
+            descRegistro: descRegistro,
+            fecha: fecha,
+            monto: monto
+          },
+          token: token,
+        });
+
+        const newId = response.data._id;
+        const newItem = { _id: newId, subtype: addNewSubtype, typevalue: typevalue };
+        const updatedData = [...data, newItem];
+        setAddNewSubtype("");
+        updateData(updatedData, typevalue);
+        refetch();
+      } catch (error) {
+        console.error("Error al agregar el nuevo valor:", error);
+      }
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setEditingId(null);
+    setNewSubtype(originalSubtype);
+  };
+
+  const openNumericKeyboard = () => {
+    setIsNumericKeyboardOpen(true);
+  };
+
+  const closeNumericKeyboard = () => {
+    setIsNumericKeyboardOpen(false);
+  };
+
+  const handleNumericButtonClick = (number: number) => {
+    setMonto((prevValue) => prevValue + number.toString());
+  };
+
+  useEffect(() => {
+    if (!editingId) {
+      setAddNewSubtype("");
+    }
+  }, [editingId]);
+
+  useEffect(() => {
+    if (editingId && tableRef.current) {
+      const rowElement = tableRef.current.querySelector(`#row-${editingId}`);
+      if (rowElement) {
+        rowElement.scrollIntoView({ behavior: "smooth", block: "center" });
+      }
+    }
+  }, [editingId, tableRef]);
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (editingId) {
+        //handleSave(editingId);
+      } else {
+        handleAdd();
+      }
+    } else if (event.key === "Escape") {
+      event.preventDefault();
+      if (editingId) {
+        handleCancelEdit();
+      }
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!monto || !fecha || !descRegistro) {
+      if (!monto) {
+        toast.error("El campo de valor numérico es obligatorio.");
+      }
+      if (!fecha) {
+        toast.error("Debes seleccionar una fecha válida.");
+      }
+      if (!descRegistro) {
+        toast.error("Debes seleccionar un tipo.");
+      }
+      return;
+    }
+
+    try {
+      // ... (lógica de envío del formulario)
+    } catch (error) {
+      console.error("Error al agregar el nuevo valor:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleAdd}>
+      <div>
+        <Typography variant="h6" gutterBottom>
+          Add {title}
+        </Typography>
+
+        <Grid container spacing={2}>
+          {/* Numeric Value */}
+          <Grid item xs={12}>
+            <TextField
+              label="Numeric Value"
+              variant="outlined"
+              type="text"
+              value={monto || ""}
+              fullWidth
+              onClick={openNumericKeyboard}
+              onKeyPress={handleKeyPress}
+            />
+          </Grid>
+
+          {/* DatePicker */}
+          <Grid item xs={12}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Select Date"
+                value={fecha}
+                onChange={(newValue) => {
+                  if (newValue !== null) {
+                    setFecha(newValue);
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </Grid>
+
+          {/* Select Type */}
+          <Grid item xs={12}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel>Type</InputLabel>
+              <Select
+                label="Type"
+                value={descRegistro}
+                onChange={(e) => setDescRegistro(e.target.value as string)}
+              >
+                {data.map((item) => (
+                  <MenuItem key={item._id} value={item.subtype}>
+                    {item.subtype}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          {/* Botón para enviar el formulario */}
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              name="iniciar"
+              id="idIniciar"
+              color="primary"
+              onClick={handleAdd} // Cambiar a onClick
+              fullWidth
+              sx={{ marginTop: 2 }}
+            >
+              Add
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+
+      <Dialog open={isNumericKeyboardOpen} onClose={closeNumericKeyboard}>
+        <DialogTitle>New Value</DialogTitle>
+        <DialogContent>
+          <div>
+            <Button onClick={() => handleNumericButtonClick(1)}>1</Button>
+            <Button onClick={() => handleNumericButtonClick(2)}>2</Button>
+            <Button onClick={() => handleNumericButtonClick(3)}>3</Button>
+          </div>
+          <div>
+            <Button onClick={() => handleNumericButtonClick(4)}>4</Button>
+            <Button onClick={() => handleNumericButtonClick(5)}>5</Button>
+            <Button onClick={() => handleNumericButtonClick(6)}>6</Button>
+          </div>
+          <div>
+            <Button onClick={() => handleNumericButtonClick(7)}>7</Button>
+            <Button onClick={() => handleNumericButtonClick(8)}>8</Button>
+            <Button onClick={() => handleNumericButtonClick(9)}>9</Button>
+          </div>
+          <div>
+            <Button></Button>
+            <Button onClick={() => handleNumericButtonClick(0)}>0</Button>
+            <Button></Button>
+          </div>
+          <div>
+            <Button onClick={() => setMonto("")}>Clear</Button>
+            <Button onClick={closeNumericKeyboard}>Close</Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </form>
+  );
+};
+
+export default TableAddRegister;
